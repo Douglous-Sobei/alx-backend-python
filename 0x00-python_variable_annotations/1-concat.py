bfa3155159389a5cbfbd4f3931f1199efcaa0dc7
Douglous Sobei <douglousmangoyi@gmail.com>
2023-07-07 04:48:51 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Handling of string with annotations
+"""
+
+
+def concat(mystring1: str, mystring2: str) -> str:
+    """
+        Args:
+            mystring1: The first string parameter
+            mystring2: The second string parameter
+        
+        Return:
+            Concanated String
+    """
+
+    return mystring1 + mystring2
