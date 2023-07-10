2ae70d39847bab9796d71952a8fd7e47cfe2039a
Douglous Sobei <douglousmangoyi@gmail.com>
2023-07-11 01:44:07 +0300

    Measure the runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
-"""
-    Croutine at the same time with sync.
-"""
-
+""" Coroutine at the same time witha sync """
+import asyncio
+import random
 from typing import List
 
 
@@ -13,11 +11,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
         Args:
-            max_delay: Max wait
-            n: Spawn function
+            max_delay: max wait
+            n: spawn function
 
         Return:
-            Float time random
+            float time random
     """
     delays: List[float] = []
     tasks: List = []
